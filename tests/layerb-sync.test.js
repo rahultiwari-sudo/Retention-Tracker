a0e1a45e73da53c1dcd3769d526e840dead055df
rahultiwari-sudo <rahul.tiwari@sellergeni.com>
2026-07-31 06:36:49 +0000

    31Jul_Sync2

--- tests/layerb-sync.test.js
+++ tests/layerb-sync.test.js
@@ -4,16 +4,19 @@ const path = require('path');
 const vm = require('vm');
 
 const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
-const match = html.match(/function initLayerB\(\) \{[\s\S]*?\n\}\nfunction normalizeLayerB\(value\) \{[\s\S]*?\n\}\nfunction layerBEquals\(a, b\) \{[\s\S]*?\n\}/);
+const match = html.match(/function initLayerB\(\) \{[\s\S]*?\n\}\nfunction getDisplayLayerB\(layerB, confirmedLayerB, pendingErrors, id\) \{[\s\S]*?\n\}\nfunction normalizeLayerB\(value\) \{[\s\S]*?\n\}\nfunction layerBEquals\(a, b\) \{[\s\S]*?\n\}/);
 
-assert.ok(match, 'layerBEquals helper should be defined in index.html');
+assert.ok(match, 'sync helper functions should be defined in index.html');
 
 const context = vm.createContext({});
 const script = new vm.Script(match[0]);
 script.runInContext(context);
 
+const getDisplayLayerB = context.getDisplayLayerB;
 const layerBEquals = context.layerBEquals;
+assert.strictEqual(getDisplayLayerB({ a: { disposition: 'Interested' } }, { a: { disposition: 'Call Later' } }, { a: true }, 'a').disposition, 'Interested');
+assert.strictEqual(getDisplayLayerB({ a: { disposition: 'Interested' } }, { a: { disposition: 'Call Later' } }, {}, 'a').disposition, 'Call Later');
 assert.strictEqual(layerBEquals({ connectedRetention: 'Yes' }, { connectedRetention: 'Yes' }), true);
 assert.strictEqual(layerBEquals({ connectedRetention: 'Yes' }, { connectedRetention: 'No' }), false);
 assert.strictEqual(layerBEquals({ remarks: ['a'] }, { remarks: ['a'] }), true);
-console.log('layerBEquals regression test passed');
+console.log('sync helper regression test passed');
